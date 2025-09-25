5bbf36ec8714b99509e15d6487ed066f3da19947
N-Viveharan <nagalingamviveharan2001@gmail.com>
2025-09-25 08:49:32 +0530

    create the navbar,

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+
+const routes=(
+  <Router>
+    <Routes>
+      <Route path='/dashboad' exact element={<Home/>}/>
+      <Route path='/login' exact element={<Login/>}/>
+      <Route path='/signup' exact element={<SignUp/>}/>
+
+    </Routes>
+
+  </Router>
+);
 
 function App() {
   return (
     <div>
-      <Home/>
+     {routes}
     </div>
   )
 }
